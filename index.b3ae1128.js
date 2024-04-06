f2a1d6833f0b62ea231344862079464d38b9517e
Oleh Tsiolkovskiy <olehtsiolkovskiy86@gmail.com>
2024-04-07 00:02:41 +0300

    make build

---
+++ index.b3ae1128.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.getElementById("form").addEventListener("submit",function(e){this.reset(),e.preventDefault()});
+//# sourceMappingURL=index.b3ae1128.js.map
